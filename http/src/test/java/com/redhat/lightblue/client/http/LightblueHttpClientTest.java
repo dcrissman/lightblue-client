aa8e08363566fcbe6305e838597b2434bdcc92f4
Dennis Crissman <dcrissma@redhat.com>
2015-02-19 13:18:58 -0500

    fixes #28
    test case for single object

--- http/src/test/java/com/redhat/lightblue/client/http/LightblueHttpClientTest.java
+++ http/src/test/java/com/redhat/lightblue/client/http/LightblueHttpClientTest.java
@@ -6,52 +6,117 @@ import static com.redhat.lightblue.client.projection.FieldProjection.includeFiel
 import java.io.IOException;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.redhat.lightblue.client.http.model.SimpleModelObject;
 import com.redhat.lightblue.client.request.data.DataFindRequest;
 
 public class LightblueHttpClientTest {
 
-	LightblueHttpClientMock client = new LightblueHttpClientMock();
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
-	@Test
-	public void testPojoMapping() throws IOException {
-		DataFindRequest findRequest = new DataFindRequest("foo", "bar");
+    LightblueHttpClientMock client = new LightblueHttpClientMock();
 
-		findRequest.where(withValue("foo = bar"));
-		findRequest.select(includeField("_id"));
+    @Test
+    public void testPojoMapping() throws IOException {
+        DataFindRequest findRequest = new DataFindRequest("foo", "bar");
 
-		client.setLightblueResponse("{\"matchCount\": 1, \"modifiedCount\": 0, \"processed\": [{\"_id\": \"idhash\", \"field\":\"value\"}], \"status\": \"COMPLETE\"}");
+        findRequest.where(withValue("foo = bar"));
+        findRequest.select(includeField("_id"));
 
-		SimpleModelObject[] results = client.data(findRequest,
-		        SimpleModelObject[].class);
+        client.setLightblueResponse("{\"matchCount\": 1, \"modifiedCount\": 0, \"processed\": [{\"_id\": \"idhash\", \"field\":\"value\"}], \"status\": \"COMPLETE\"}]");
 
-		Assert.assertEquals(1, results.length);
-		;
-		Assert.assertTrue(new SimpleModelObject("idhash", "value")
-		        .equals(results[0]));
-	}
+        SimpleModelObject[] results = client.data(findRequest,
+                SimpleModelObject[].class);
 
-	@Test
-	public void testPojoMappingWithParsingError() throws IOException {
-		DataFindRequest findRequest = new DataFindRequest("foo", "bar");
+        Assert.assertEquals(1, results.length);
 
-		findRequest.where(withValue("foo = bar"));
-		findRequest.select(includeField("_id"));
+        Assert.assertTrue(new SimpleModelObject("idhash", "value").equals(results[0]));
+    }
 
-		String response = "{\"context\": \"rest/FindCommand/esbMessage\", \"errorCode\": \"rest-crud:RestFindError\", \"msg\": \"java.lang.IllegalArgumentException: Cannot call method public static com.redhat.lightblue.crud.FindRequest com.redhat.lightblue.crud.FindRequest.fromJson(com.fasterxml.jackson.databind.node.ObjectNode)\", \"objectType\": \"error\"}";
+    @Test
+    public void testPojoMappingWithErrorInResponse() throws IOException {
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Error in lightblue response");
 
-		client.setLightblueResponse(response);
+        DataFindRequest findRequest = new DataFindRequest("foo", "bar");
 
-		try {
-			client.data(findRequest, SimpleModelObject[].class);
+        findRequest.where(withValue("foo = bar"));
+        findRequest.select(includeField("_id"));
 
-			Assert.fail();
-		} catch (RuntimeException e) {
-			Assert.assertTrue(e.getMessage().startsWith("Error parsing lightblue response"));
-		}
+        String response = "{\"context\": \"rest/FindCommand/esbMessage\", \"errorCode\": \"rest-crud:RestFindError\", \"msg\": \"java.lang.IllegalArgumentException: Cannot call method public static com.redhat.lightblue.crud.FindRequest com.redhat.lightblue.crud.FindRequest.fromJson(com.fasterxml.jackson.databind.node.ObjectNode)\", \"objectType\": \"error\"}";
 
-	}
+        client.setLightblueResponse(response);
+
+        client.data(findRequest, SimpleModelObject[].class);
+    }
+
+    /**
+     * This is bad json data in order to test a scenario.
+     */
+    @Test
+    public void testPojoMappingWithParsingError() throws IOException {
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Error parsing lightblue response");
+
+        DataFindRequest findRequest = new DataFindRequest("foo", "bar");
+
+        findRequest.where(withValue("foo = bar"));
+        findRequest.select(includeField("_id"));
+
+        String response = "{\"processed\": \"bad json\"}";
+
+        client.setLightblueResponse(response);
+
+        client.data(findRequest, SimpleModelObject[].class);
+    }
+
+    @Test
+    public void testPojoMappingWithEmptyProcessedResults() throws IOException {
+        DataFindRequest findRequest = new DataFindRequest("foo", "bar");
+
+        findRequest.where(withValue("foo = bar"));
+        findRequest.select(includeField("_id"));
+
+        client.setLightblueResponse("{\"matchCount\": 0, \"modifiedCount\": 0}");
+
+        SimpleModelObject[] results = client.data(findRequest,
+                SimpleModelObject[].class);
+
+        Assert.assertNull(results);
+    }
+
+    @Test
+    public void testPojoMappingWithEmptyProcessedResults_EmptyArrayNode() throws IOException {
+        DataFindRequest findRequest = new DataFindRequest("foo", "bar");
+
+        findRequest.where(withValue("foo = bar"));
+        findRequest.select(includeField("_id"));
+
+        client.setLightblueResponse("{\"status\":\"COMPLETE\",\"modifiedCount\":0,\"matchCount\":0,\"processed\":[]}");
+
+        SimpleModelObject[] results = client.data(findRequest,
+                SimpleModelObject[].class);
+
+        Assert.assertEquals(0, results.length);
+    }
+
+    @Test
+    public void testPojoMappingWithEmptyProcessedResults_ForSingleResult() throws IOException {
+        DataFindRequest findRequest = new DataFindRequest("foo", "bar");
+
+        findRequest.where(withValue("foo = bar"));
+        findRequest.select(includeField("_id"));
+
+        client.setLightblueResponse("{\"status\":\"COMPLETE\",\"modifiedCount\":0,\"matchCount\":0,\"processed\":[]}");
+
+        SimpleModelObject result = client.data(findRequest,
+                SimpleModelObject.class);
+
+        Assert.assertNull(result);
+    }
 
 }
